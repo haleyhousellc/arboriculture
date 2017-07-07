5c3a4ce5649fa962fa0d3c6594ae0eea8c494450
Graham Haley <graham.andrew.haley@gmail.com>
2017-07-07 10:17:48 -0700

    having trouble with publishing versions and npm

--- src/util.ts
+++ src/util.ts
@@ -20,7 +20,7 @@ export type UnionKeyToValue<U extends string> = { [K in U]: K; };
 /**
  * The current package version
  */
-export const VERSION: string    = '0.1.0';
+export const VERSION: string    = '0.1.2';
 
 /**
  * The user agent string
