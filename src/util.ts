c5b66d34e50c45f252fe58a4aab0142a91804b68
Graham Haley <graham.andrew.haley@gmail.com>
2017-07-07 10:26:19 -0700

    versioning woes should be fixed

--- src/util.ts
+++ src/util.ts
@@ -20,7 +20,7 @@ export type UnionKeyToValue<U extends string> = { [K in U]: K; };
 /**
  * The current package version
  */
-export const VERSION: string    = '0.1.2';
+export const VERSION: string    = '0.1.3';
 
 /**
  * The user agent string
